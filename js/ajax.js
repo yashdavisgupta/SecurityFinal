1f124472c6b1bfb9502ee5e0291b20a080c858c0
yashdavisgupta <yashdavisgupta@gmail.com>
2017-05-03 15:25:20 -0500

    update materialize for new code.

--- js/ajax.js
+++ js/ajax.js
@@ -11,12 +11,12 @@ function currentServer()
     		url: "ajax.php?request=currentServer",
     		contentType: "application/json; charset=utf-8",
     		dataType: "json",
-    		success: function(data){ 
+    		success: function(data){
 			jQuery(document).ready(function() {
 				console.log("Successful JSON Response!");
 				console.log(data[0].name + ", " + data[0].ip);
 				jQuery('#currentServer').html('<b>Current Server</b><p>Name: ' + data[0].name + '</p><p>IP: ' + data[0].ip + '</p>');
-			}); 
+			});
 		},
     		failure: function(errMsg) {
         		alert(errMsg);
@@ -34,7 +34,7 @@ function addServer(name, ip)
     		url: "ajax.php?request=addServer&second=" + name + "&third=" + ip,
     		contentType: "application/json; charset=utf-8",
     		dataType: "json",
-    		success: function(data){ 
+    		success: function(data){
 			console.log("Worked - " + data[0].worked);
 			showServers();
 		},
@@ -44,15 +44,15 @@ function addServer(name, ip)
 	});
 }
 
-function showServers() 
+function showServers()
 {
 	$.ajax({
    	 	type: "GET",
     		url: "ajax.php?request=servers",
     		contentType: "application/json; charset=utf-8",
     		dataType: "json",
-    		success: function(data){ 
-			update(data); 
+    		success: function(data){
+			update(data);
 		},
     		failure: function(errMsg) {
         		alert(errMsg);
@@ -67,13 +67,13 @@ function ping(ip, id)
     		url: "ajax.php?request=ping&second=" + ip,
     		contentType: "application/json; charset=utf-8",
     		dataType: "json",
-    		success: function(data){ 
+    		success: function(data){
 			console.log("Successful JSON Response!");
 
 			jQuery(document).ready(function() {
 				jQuery('#'+id).html("");
 				jQuery('#'+id).append('<div id="' + id + '"> <p>Packets Transmitted: '+ data[0].transmitted + '</p> <p>Packets Received: ' + data[0].received + '</p> <p>Packets Lost: ' + data[0].lost + '</p> <p>Time: ' + data[0].time + 'ms</p> <p>Time Min: ' + data[0].min + 'ms</p> <p>Time Max: ' + data[0].max + 'ms</p> <p>Time Avg: ' + data[0].avg + 'ms</p></div>');
-			}); 
+			});
 		},
     		failure: function(errMsg) {
         		alert(errMsg);
@@ -88,9 +88,9 @@ function setCurrentServer(pkey)
     		url: "ajax.php?request=setCS&second=" + pkey,
     		contentType: "application/json; charset=utf-8",
     		dataType: "json",
-    		success: function(data){ 
+    		success: function(data){
 			console.log("Worked - " + data[0].worked);
-			currentServer(); 
+			currentServer();
 		},
     		failure: function(errMsg) {
         		alert(errMsg);
@@ -107,30 +107,18 @@ function update(response)
 	{
 		jQuery('#servers').html("");
 		for(i = 0; i < response.length; i++)
-		{	
+		{
 			jQuery('#servers').append('<div id="'+ response[i].pkey +'"> <p>'+ response[i].name + '</p> <p>' + response[i].ip + '</p>');
-			
-			var str1 = '<input type="button" value="Ping" style="display: inline;" onclick="ping(\'';
+
+			var str1 = '<input type="button" value="Ping" class="btn waves-effect waves-light" onclick="ping(\'';
 			str1 = str1.concat(response[i].ip);
 			str1 = str1.concat('\', \'');
 			str1 = str1.concat(response[i].pkey);
 			str1 = str1.concat('info\')">');
 			console.log(str1);
 			jQuery('#'+response[i].pkey).append(str1);
-			jQuery('#'+response[i].pkey).append('<input type="button" value="Select Server" style="display: inline;" onclick="setCurrentServer('+ response[i].pkey +')">');
-			jQuery('#'+response[i].pkey).append('<div id="' + response[i].pkey + 'info"></div></div>');		
+			jQuery('#'+response[i].pkey).append('<input type="button" value="Select Server" class="btn waves-effect waves-light" onclick="setCurrentServer('+ response[i].pkey +')">');
+			jQuery('#'+response[i].pkey).append('<div id="' + response[i].pkey + 'info"></div></div>');
 		}
 	});
 }
-
-
-
-
-
-
-
-
-
-
-
-
